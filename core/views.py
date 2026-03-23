d4287406b11f279cc8fda02096dec9bd3d5ef376
Lynyrd <lqye0107@gmailcom>
2026-03-23 10:51:45 +0100

    add core urls views and basic home and menu templates

--- core/views.py
+++ core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home_view(request):
+    return render(request, 'core/home.html')
+
+def menu_view(request):
+    return render(request, 'core/menu.html')
