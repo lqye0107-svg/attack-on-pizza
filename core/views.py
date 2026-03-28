aca5de24110637cfe357ce0228549d6c2ca0bd0e
Lynyrd <lqye0107@gmailcom>
2026-03-28 12:13:34 +0100

    Forms created and updated urls and views

--- core/views.py
+++ core/views.py
@@ -1,8 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib import messages
+from .forms import RegisterForm
 
-# Create your views here.
+
 def home_view(request):
     return render(request, 'core/home.html')
 
+
 def menu_view(request):
     return render(request, 'core/menu.html')
+
+
+def register_view(request):
+    if request.user.is_authenticated:
+        return redirect('menu')
+
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'Registration successful. Welcome to Attack on Pizza!')
+            return redirect('menu')
+    else:
+        form = RegisterForm()
+
+    return render(request, 'core/register.html', {'form': form})
+
+
+class CustomLoginView(LoginView):
+    template_name = 'core/login.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('menu')
+        return super().dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        messages.success(self.request, 'You have logged in successfully.')
+        return super().form_valid(form)
+
+
+class CustomLogoutView(LogoutView):
+    def dispatch(self, request, *args, **kwargs):
+        messages.success(request, 'You have logged out successfully.')
+        return super().dispatch(request, *args, **kwargs)
